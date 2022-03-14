c671806f516aadea2d0c25320bfa62af18bbba70
Max <maxime-obry@hotmail.fr>
2022-03-14 17:40:57 +0100

    templatelocation

--- main.go
+++ main.go
@@ -20,6 +20,10 @@ type Bloup1 struct {
 	Res1 []Viewpeople
 }
 
+type Bloup2 struct {
+	Res2 []ViewLocation
+}
+
 type Viewpeople struct {
 	ID        string   `json:"id"`
 	Name      string   `json:"name"`
@@ -51,6 +55,17 @@ type ViewData struct {
 	Locations              []string `json:"locations"`
 	Vehicles               []string `json:"vehicles"`
 	URL                    string   `json:"url"`
+}
+
+type ViewLocation struct {
+	ID           string   `json:"id"`
+	Name         string   `json:"name"`
+	Climate      string   `json:"climate"`
+	Terrain      string   `json:"terrain"`
+	SurfaceWater string   `json:"surface_water"`
+	Residents    []string `json:"residents"`
+	Films        []string `json:"films"`
+	URL          string   `json:"url"`
 }
 
 func loadAPI() []ViewData {
@@ -219,15 +234,101 @@ func loadAPIpeople1(id string) Viewpeople {
 	return vd
 }
 
+func loadAPILocation1() []ViewLocation {
+	var vd []ViewLocation
+
+	url := "https://ghibliapi.herokuapp.com/locations/"
+
+	httpClient := http.Client{
+		Timeout: time.Second * 2, // define timeout
+	}
+
+	//create request
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "API AT test <3")
+
+	//make api call
+	res, getErr := httpClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	//parse response
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	jsonErr := json.Unmarshal(body, &vd)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return vd
+}
+
+func loadAPILocation2(id string) ViewLocation {
+	var vd ViewLocation
+
+	url := "https://ghibliapi.herokuapp.com/locations/" + id
+
+	httpClient := http.Client{
+		Timeout: time.Second * 2, // define timeout
+	}
+
+	//create request
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "API AT test <3")
+
+	//make api call
+	res, getErr := httpClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	//parse response
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	jsonErr := json.Unmarshal(body, &vd)
+	if jsonErr != nil {
+		debug.PrintStack()
+		log.Fatal(jsonErr)
+	}
+
+	return vd
+}
+
 func main() {
 	viewData := Bloup{Res: loadAPI()}
 	viewDatapers := Bloup1{Res1: loadAPIpeople()}
+	ViewDatalocation := Bloup2{Res2: loadAPILocation1()}
 	//viewData := loadAPI()
 	tmplpage := template.Must(template.ParseFiles("page/page1.html"))
 	tmplpage1 := template.Must(template.ParseFiles("page/movies.html"))
 	tmplpage2 := template.Must(template.ParseFiles("page/descrmovie.html"))
 	tmplpage4 := template.Must(template.ParseFiles("page/people.html"))
 	tmplpage3 := template.Must(template.ParseFiles("page/descrpeople.html"))
+	tmplpage5 := template.Must(template.ParseFiles("page/location.html"))
+	tmplpage6 := template.Must(template.ParseFiles("page/descrlocation.html"))
 
 	fs := http.FileServer(http.Dir("./style"))
 	http.Handle("/style/", http.StripPrefix("/style/", fs))
@@ -244,6 +345,10 @@ func main() {
 		tmplpage1.Execute(w, viewData)
 	})
 
+	http.HandleFunc("/location", func(w http.ResponseWriter, r *http.Request) {
+		tmplpage5.Execute(w, ViewDatalocation)
+	})
+
 	http.HandleFunc("/descrmovie/", func(w http.ResponseWriter, r *http.Request) {
 		id := strings.ReplaceAll(r.URL.Path, "/descrmovie/", "")
 		viewData1 := loadAPI2(id)
@@ -256,8 +361,13 @@ func main() {
 		tmplpage3.Execute(w, viewDatapers1)
 	})
 
+	http.HandleFunc("/descrlocation/", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.ReplaceAll(r.URL.Path, "/descrlocation/", "")
+		viewDataLocation1 := loadAPILocation2(id)
+		tmplpage6.Execute(w, viewDataLocation1)
+	})
+
 	fmt.Println("Starting server on port 80")
-	print("error")
 	http.ListenAndServe(":8000", nil)
 }
 
